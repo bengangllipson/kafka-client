37b77f5cdd13bdb47416c77d6416931134353a3a
bengangllipson <sonofblip@gmail.com>
2026-02-13 14:35:03 -0600

    make it async

--- src/main/kotlin/com/bengangllipson/kafkaclient/KafkaConsumer.kt
+++ src/main/kotlin/com/bengangllipson/kafkaclient/KafkaConsumer.kt
@@ -1,17 +1,25 @@
 package com.bengangllipson.kafkaclient
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlin.time.Duration.Companion.milliseconds
 
 class KafkaConsumer {
     suspend fun process() {
-        listOf(1, 2, 3)
-            .map { i ->
-                println("processing $i")
-                delay((0 until 50).random().milliseconds)
-                "processed $i"
-            }
-            .onEach { result -> println(result) }
+        coroutineScope {
+            listOf(1, 2, 3)
+                .map { i ->
+                    async {
+                        println("processing $i")
+                        delay((0 until 50).random().milliseconds)
+                        "processed $i"
+                    }
+                }
+                .awaitAll()
+                .onEach { result -> println(result) }
+        }
     }
 }
 
