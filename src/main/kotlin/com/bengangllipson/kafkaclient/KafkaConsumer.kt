6a1e7318af9de51f0292600efa9c69e859645682
bengangllipson <sonofblip@gmail.com>
2026-02-13 14:39:18 -0600

    introduce problems, process related events out of order and process batches synchronously

--- src/main/kotlin/com/bengangllipson/kafkaclient/KafkaConsumer.kt
+++ src/main/kotlin/com/bengangllipson/kafkaclient/KafkaConsumer.kt
@@ -6,23 +6,29 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlin.time.Duration.Companion.milliseconds
 
+data class Payload(
+    val key: String, val value: Int
+)
+
 class KafkaConsumer {
     suspend fun process() {
         coroutineScope {
-            listOf(1, 2, 3)
-                .map { i ->
+            listOf(
+                listOf(Payload("a", 1), Payload("b", 2), Payload("a", 3)),
+                listOf(Payload("c", 4), Payload("b", 5), Payload("d", 6)),
+            ).flatMap { batch ->
+                batch.map { i ->
                     async {
                         println("processing $i")
                         delay((0 until 50).random().milliseconds)
                         "processed $i"
                     }
-                }
-                .awaitAll()
-                .onEach { result -> println(result) }
+                }.awaitAll()
+            }.onEach { result -> println(result) }
         }
     }
 }
 
-suspend fun main() {
-    KafkaConsumer().process()
-}
+    suspend fun main() {
+        KafkaConsumer().process()
+    }
